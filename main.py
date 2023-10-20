787fe99b89e2e92071a4273114e8da7824fcb788
laisuk <laisuk@yahoo.com>
2023-10-20 09:12:06 +0800

    add char code detection

--- main.py
+++ main.py
@@ -9,6 +9,19 @@ def paste_input():
     source_texbox.delete("1.0", tk.END)
     source_texbox.insert("1.0", text)
 
+    test_text = text if len(text) < 100 else text[0:100]
+    
+    match check_textcode(test_text):
+        case 1:
+            source_charcode_label.config(text="zh-Hant (繁体)")
+            config_option.set(value="t2s")
+        case 2:
+            source_charcode_label.config(text="zh-Hans (简体)")
+            config_option.set(value="s2t")
+        case _:
+            source_charcode_label.config(text="Non-zh")
+    
+
 
 def copy_output():
     pc.copy(destination_textbox.get("1.0", tk.END))
@@ -18,12 +31,28 @@ def convert():
     input_text = source_texbox.get("1.0", tk.END)
     if config_option.get() == "jieba":
         segment_list = jieba.cut(input_text)
-        output_text = "/ ".join(segment_list)
+        output_text = "/".join(segment_list)
     else:
         converter = OpenCC(config_option.get())
         output_text = converter.convert(input_text)
     destination_textbox.delete("1.0", tk.END)
     destination_textbox.insert("1.0", output_text)
+
+    if config_option.get() != "jieba" and source_charcode_label.cget("text") != "Non-zh":
+        destination_charcode_label.config(text="zh-Hant (繁体)" if config_option.get()=="s2t" else "zh-Hans (简体)")
+    else:
+        destination_charcode_label.config(text=source_charcode_label.cget("text"))
+
+
+def check_textcode(text):
+    converted_text = OpenCC("t2s").convert(text)
+    if(converted_text != text):
+        return 1
+    elif(OpenCC("s2t").convert(converted_text) == text):
+        return 0
+    else:
+        return 2
+
 
 
 window = tk.Tk()
@@ -42,8 +71,7 @@ t2s_radiobutton = tk.Radiobutton(
 s2t_radiobutton = tk.Radiobutton(
     config_labelframe, text="zh-Hans (简体) to zh-hant (繁体)", padx=20, pady=5, value="s2t", variable=config_option,
     font="Arial 12")
-jieba_radiobutton = tk.Radiobutton(config_labelframe, text="Words Segmentor (拆词)", padx=20, pady=5, value="jieba",
-                                   variable=config_option, font="Arial 12")
+jieba_radiobutton = tk.Radiobutton(config_labelframe, text="Words Segmentor (拆词)", padx=20, pady=5, value="jieba", variable=config_option, font="Arial 12")
 
 t2s_radiobutton.grid(row=0, column=0)
 s2t_radiobutton.grid(row=0, column=1)
@@ -75,23 +103,28 @@ destination_labelframe.grid(
     row=1, column=2, sticky="news", padx=10, pady=5, columnspan=2)
 source_label = tk.Label(source_labelframe, text="Source")
 destination_label = tk.Label(destination_labelframe, text="Destination")
-source_label.grid(row=1, column=0, padx=5, pady=5)
-destination_label.grid(row=1, column=0, padx=5, pady=5)
+source_label.grid(row=0, column=0, padx=5, pady=5)
+destination_label.grid(row=0, column=0, padx=5, pady=5)
 paste_button = tk.Button(
-    source_labelframe, text="Paste Input", command=paste_input, font="Arial 8 bold")
+    source_labelframe, text=" Paste Input ", command=paste_input, font="Arial 8 bold")
 copy_button = tk.Button(destination_labelframe,
-                        text="Copy Output", command=copy_output, font="Arial 8 bold")
-paste_button.grid(row=1, column=1, padx=5, pady=5)
-copy_button.grid(row=1, column=2, padx=5, pady=5)
+                        text=" Copy Output ", command=copy_output, font="Arial 8 bold")
+
+source_charcode_label = tk.Label(source_labelframe, text="None")
+destination_charcode_label = tk.Label(destination_labelframe, text="None")
+paste_button.grid(row=0, column=1, padx=5, pady=5)
+copy_button.grid(row=0, column=1, padx=5, pady=5)
+source_charcode_label.grid(row=0, column=2, padx=5, pady=5)
+destination_charcode_label.grid(row=0, column=2, padx=5, pady=5)
 
 action_labelframe = tk.LabelFrame(frame)
 convert_button = tk.Button(
-    action_labelframe, text="Convert", command=convert, font="Arial 12 bold")
+    action_labelframe, text=" Convert ", command=convert, font="Arial 12 bold")
 action_labelframe.grid(row=2, column=0, sticky="news", padx=20, pady=5)
 convert_button.grid(row=0, column=0, padx=395, pady=5)
-exit_button = tk.Button(action_labelframe, text="Exit",
+exit_button = tk.Button(action_labelframe, text=" Exit ",
                         command=window.destroy, font="Arial 12 bold")
-exit_button.place(relx=0.98, rely=0.5, anchor="e")
+exit_button.place(relx=0.99, rely=0.5, anchor="e")
 
 window.eval('tk::PlaceWindow . center')
 window.mainloop()
