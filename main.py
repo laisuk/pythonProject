0122dd7de547aa94cc1421ae3fdad659f976fc13
laisuk <laisuk@yahoo.com>
2024-01-11 14:18:22 +0800

    Add Punctuantions conversion

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
+import os.path
 import re
 import tkinter as tk  # GUI
 import jieba  # Segmentor module
 import pyperclip as pc  # Clipboard module
 from opencc import OpenCC  # use module: pip install -u opencc-python-reimplemented
-from tkinter import filedialog as fd
+from tkinter.filedialog import askopenfilename
 
 
 def paste_input():
@@ -17,14 +18,15 @@ def paste_input():
     match check_textcode(test_text):
         case 1:
             source_charcode_label.config(text="zh-Hant (繁体)")
-            config_option.set(value="tw2sp")
+            config_option.set(value="tw2s")
         case 2:
             source_charcode_label.config(text="zh-Hans (简体)")
-            config_option.set(value="s2twp")
+            config_option.set(value="s2tw")
         case _:
             source_charcode_label.config(text="Non-zh (其它)")
 
     source_charcount_label.config(text=f"( {len(text):,} Chars )")
+    filename_label.config(text="")
 
 
 def copy_output():
@@ -32,8 +34,12 @@ def copy_output():
 
 
 def openfile():
-    filename = fd.askopenfilename(initialdir="./", title="Open File", filetypes=(
+    filename = askopenfilename(initialdir="./", title="Open File", filetypes=(
         ("Text Files", "*.txt"), ("Subtitle Files", "*.srt;*.vtt;*.ass;*.ttml2;*.xml"), ("All Files", "*.*")))
+
+    if not filename:
+        return
+
     with open(filename, "r", encoding="utf-8") as f:
         contents = f.read()
 
@@ -46,14 +52,15 @@ def openfile():
     match check_textcode(test_text):
         case 1:
             source_charcode_label.config(text="zh-Hant (繁体)")
-            config_option.set(value="tw2sp")
+            config_option.set(value="tw2s")
         case 2:
             source_charcode_label.config(text="zh-Hans (简体)")
-            config_option.set(value="s2twp")
+            config_option.set(value="s2tw")
         case _:
             source_charcode_label.config(text="Non-zh (其它)")
 
     source_charcount_label.config(text=f"( {len(contents):,} Chars )")
+    filename_label.config(text=os.path.basename(filename))
 
 
 def convert():
@@ -62,8 +69,12 @@ def convert():
         segment_list = jieba.cut(input_text)
         output_text = "/".join(segment_list)
     else:
-        converter = OpenCC(config_option.get())
+        converter = OpenCC(config_option.get() + "p" if zhtw_option.get() else config_option.get())
         output_text = converter.convert(input_text)
+
+    if punctuation_option.get():
+        output_text = convert_punctuation(output_text, config_option.get())
+
     destination_textbox.delete("1.0", tk.END)
     destination_textbox.insert("1.0", output_text)
 
@@ -85,6 +96,30 @@ def check_textcode(text):
         return 0
 
 
+def convert_punctuation(input_text, config):
+    # Use the re module to replace the characters with their mappings
+    import re
+    # Declare a dictionary to store the characters and their mappings
+    s2t_punctuation_chars = {
+        '“': '「',
+        '”': '」',
+        '‘': '『',
+        '’': '』'
+    }
+    # Use the join method to create the regular expression patterns
+    if config[0] == "s":
+        pattern = f"[{''.join(s2t_punctuation_chars.keys())}]"  # "[“”‘’]"
+        output_text = re.sub(pattern, lambda m: s2t_punctuation_chars[m.group(0)], input_text)
+    else:
+        # Use the dict comprehension to reverse the dictionary
+        t2s_punctuation_chars = {v: k for k, v in s2t_punctuation_chars.items()}
+        pattern = f"[{''.join(t2s_punctuation_chars.keys())}]"  # "[「」『』]"
+        output_text = re.sub(pattern, lambda m: t2s_punctuation_chars[m.group(0)], input_text)
+
+    return output_text
+
+
+# === Main Window === #
 window = tk.Tk()
 window.title("Hans <-> Hant Converter")
 
@@ -93,20 +128,26 @@ frame.pack()
 
 config_labelframe = tk.LabelFrame(frame, text="Configuration")
 config_labelframe.grid(row=0, column=0, padx=20, pady=5, sticky="news")
-config_option = tk.StringVar(value="tw2sp")
+config_option = tk.StringVar(value="tw2s")
+zhtw_option = tk.IntVar(value=1)
+punctuation_option = tk.IntVar(value=1)
 
 t2s_radiobutton = tk.Radiobutton(
-    config_labelframe, text="zh-Hant (繁体) to zh-Hans (简体)", padx=20, pady=5, value="tw2sp", variable=config_option,
+    config_labelframe, text="zh-Hant (繁体) to zh-Hans (简体)", padx=20, pady=5, value="tw2s", variable=config_option,
     font="Arial 12")
 s2t_radiobutton = tk.Radiobutton(
-    config_labelframe, text="zh-Hans (简体) to zh-hant (繁体)", padx=20, pady=5, value="s2twp", variable=config_option,
+    config_labelframe, text="zh-Hans (简体) to zh-hant (繁体)", padx=20, pady=5, value="s2tw", variable=config_option,
     font="Arial 12")
 jieba_radiobutton = tk.Radiobutton(config_labelframe, text="Words Segmentor (拆词)", padx=20, pady=5, value="jieba",
                                    variable=config_option, font="Arial 12")
+zhtw_checkbutton = tk.Checkbutton(config_labelframe, text="CN/TW Idioms (中台惯用语)", variable=zhtw_option, font="Arial 10")
+punctuation_checkbutton = tk.Checkbutton(config_labelframe, text="Punctuation (标点符号)", variable=punctuation_option, font="Arial 10")
 
 t2s_radiobutton.grid(row=0, column=0)
 s2t_radiobutton.grid(row=0, column=1)
 jieba_radiobutton.grid(row=0, column=2)
+zhtw_checkbutton.grid(row=1, column=0)
+punctuation_checkbutton.grid(row=1, column=1)
 
 content_labelframe = tk.LabelFrame(frame, text="Contents")
 content_labelframe.grid(row=1, column=0, padx=20, pady=5, sticky="news")
@@ -137,9 +178,9 @@ destination_label = tk.Label(destination_labelframe, text="Destination")
 source_label.grid(row=0, column=0, padx=5, pady=5)
 destination_label.grid(row=0, column=0, padx=5, pady=5)
 paste_button = tk.Button(
-    source_labelframe, text=" Paste Input ", command=paste_input, font="Arial 8 bold")
+    source_labelframe, text=" Paste ", command=paste_input, font="Arial 8 bold")
 copy_button = tk.Button(destination_labelframe,
-                        text=" Copy Output ", command=copy_output, font="Arial 8 bold")
+                        text=" Copy ", command=copy_output, font="Arial 8 bold")
 
 source_charcode_label = tk.Label(source_labelframe, text="None")
 destination_charcode_label = tk.Label(destination_labelframe, text="None")
@@ -155,10 +196,12 @@ action_labelframe = tk.LabelFrame(frame)
 action_labelframe.grid(row=2, column=0, sticky="news", padx=20, pady=(0, 20))
 openfile_button = tk.Button(
     action_labelframe, text=" Open File ", command=openfile, font="Arial 10 bold")
+filename_label = tk.Label(action_labelframe, text="")
 convert_button = tk.Button(
     action_labelframe, text=" Convert ", command=convert, font="Arial 12 bold")
 convert_button.grid(row=0, column=0, padx=390, pady=5)
 openfile_button.place(relx=0.01, rely=0.5, anchor="w")
+filename_label.place(relx=0.11, rely=0.5, anchor="w")
 exit_button = tk.Button(action_labelframe, text=" Exit ",
                         command=window.destroy, font="Arial 10 bold")
 exit_button.place(relx=0.99, rely=0.5, anchor="e")
